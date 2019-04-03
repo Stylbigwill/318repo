f949f12828990fd50fa53e6178a669fac3f75305
wward <wward@wward-1pa.cmc.com>
2019-04-03 12:37:56 -0600

    Added null pointer handling.

--- src/main/java/com/henryschein/dataservices/service/impl/DataServiceImpl.java
+++ src/main/java/com/henryschein/dataservices/service/impl/DataServiceImpl.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.StringReader;
 import javax.ws.rs.Path;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.henryschein.dataservices.service.IDataService;
 
 @Path("/")
@@ -11,18 +13,22 @@ public class DataServiceImpl implements IDataService {
 
 	@Override
 	public String reformat(String unformattedString) throws Exception {
-		String line;
-		BufferedReader br = new BufferedReader(new StringReader(unformattedString));
-		StringBuilder sb = new StringBuilder();
-		while ((line = br.readLine()) != null) {
-			line = line.replaceFirst("\"", "[");
-			line = line.replace("\",\"", "] [");
-			line = line.replace("\",", "] [");
-			line = line.replace(",\"", "] [");
-			line = line.replace("\"", "]");
-			sb.append(line + "\n");
+		if (StringUtils.isNotBlank(unformattedString)) {
+			String line;
+			BufferedReader br = new BufferedReader(new StringReader(unformattedString));
+			StringBuilder sb = new StringBuilder();
+			while ((line = br.readLine()) != null) {
+				line = line.replaceFirst("\"", "[");
+				line = line.replace("\",\"", "] [");
+				line = line.replace("\",", "] [");
+				line = line.replace(",\"", "] [");
+				line = line.replace("\"", "]");
+				sb.append(line + "\n");
+			}
+			return sb.toString();
+		} else {
+			throw new Exception("Value to be formatted can not be empty");
 		}
-		return sb.toString();
 	}
 
 }
